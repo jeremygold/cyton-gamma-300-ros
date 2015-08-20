06e8657064d9c8b68e8f4491958914437471d853
Jeremy Gold <jeremy.gold@ikegps.com>
2015-08-20 15:23:39 +1200

    Tidy up

--- ROS_ws/src/move.py
+++ ROS_ws/src/move.py
@@ -11,15 +11,13 @@ shoulder_pitch = rospy.Publisher('/shoulder_pitch_position_controller/command', 
 wrist_pitch = rospy.Publisher('/wrist_pitch_position_controller/command', std_msgs.msg.Float64, queue_size=10)
 # gripper_position = rospy.Publisher('/gripper_position_controller/command', std_msgs.msg.Float64, queue_size=10)
 
+ # * /wrist_roll_position_controller/command [unknown type]
  # * /wrist_pitch_position_controller/command [unknown type]
  # * /elbow_yaw_position_controller/command [unknown type]
- # * /cyton_joint_trajectory_action_controller/follow_joint_trajectory/cancel [unknown type]
  # * /elbow_pitch_position_controller/command [unknown type]
- # * /wrist_roll_position_controller/command [unknown type]
  # * /shoulder_pitch_position_controller/command [unknown type]
  # * /shoulder_yaw_position_controller/command [unknown type]
  # * /shoulder_roll_position_controller/command [unknown type]
- # * /cyton_joint_trajectory_action_controller/command [unknown type]
  # * /gripper_position_controller/command [unknown type]
 
 for i in range (1,4):
